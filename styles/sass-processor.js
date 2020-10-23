b6262963b1fa48b83c66db4d63f6b2d12770bddb
andrew <andrewwaite@silktide.com>
2020-10-24 00:02:01 +0100

    Sass minification, combined bootstrap into our file to reduce network transfers

--- styles/sass-processor.js
+++ styles/sass-processor.js
@@ -1,19 +1,21 @@
 const sass = require('sass');
 const fs = require('fs-extra');
 const path = require('path');
+const CleanCss = require('clean-css');
 
 module.exports = (scssPath, cssPath) => {
 
     //If cssPath directory doesn't exist...
     if(!fs.existsSync(path.dirname(cssPath))) {
         //Encapsulate rendered css from scssPath into result variable
-        const result = sass.renderSync({file: scssPath});
+        const compiled = sass.renderSync({file: scssPath, outputStyle: "compressed", sourceComments: false});
+        const result = new CleanCss({}).minify(compiled.css.toString()).styles;
         //Create cssPath directory recursively
         fs.mkdirp(path.dirname(cssPath), {recursive: true}, (err) => {
             if (err) {
                 return console.error(err);
             }
-            fs.writeFile(cssPath, result.css.toString());
+            fs.writeFile(cssPath, result);
             console.log("Compiled SCSS successfully.")
         });
     }
@@ -22,9 +24,10 @@ module.exports = (scssPath, cssPath) => {
     // fs.watch(path.dirname(scssPath), () => {
     //     console.log(`Watching ${path.dirname(scssPath)}...`);
     //     //Encapsulate rendered css from scssPath into watchResult variable
-    //     const watchResult = sass.renderSync({file: scssPath});
+    //     const compiled = sass.renderSync({file: scssPath, outputStyle: "compressed", sourceComments: false});
+    //     const watchResult = new CleanCss({}).minify(compiled.css.toString()).styles;
     //     //Then write result css string to cssPath file
-    //     fs.writeFile(cssPath, watchResult.css.toString())
+    //     fs.writeFile(cssPath, watchResult)
     //         .catch(error => console.error(error))
     // });
 }
